29b4c65b9a275ea3c87263ca5350e10e8b5db232
sneakypete81 <sneakypete81@gmail.com>
2013-02-17 16:12:15 +0000

    Update comment

--- openphoto/api_album.py
+++ openphoto/api_album.py
@@ -41,7 +41,7 @@ class ApiAlbum:
         album.update(**kwds)
 
         # Don't return the album, since the API currently doesn't give us the modified album
-        # Uncomment the following once frontend issue #937 is resolved
+        # TODO: Uncomment the following once frontend issue #937 is resolved
 #        return album
 
     def view(self, album, **kwds):
