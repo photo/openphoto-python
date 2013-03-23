2abf46c0e6d14bdd10b19ad2fe2fff9b9ccd0b55
sneakypete81 <sneakypete81@gmail.com>
2013-03-23 16:46:47 +0000

    Prettier printout when running tests

--- tests/test_base.py
+++ tests/test_base.py
@@ -62,7 +62,7 @@ class TestBase(unittest.TestCase):
         self.photos = self.client.photos.list()
         if len(self.photos) != 3:
 #            print self.photos
-            print "[Regenerating Photos]"
+            print "\n[Regenerating Photos]"
             if len(self.photos) > 0:
                 self._delete_all()
             self._create_test_photos()
@@ -72,7 +72,7 @@ class TestBase(unittest.TestCase):
         if (len(self.tags) != 1 or
                 self.tags[0].id != self.TEST_TAG or
                 self.tags[0].count != 3):
-            print "[Regenerating Tags]"
+            print "\n[Regenerating Tags]"
             self._delete_all()
             self._create_test_photos()
             self.photos = self.client.photos.list()
@@ -85,7 +85,7 @@ class TestBase(unittest.TestCase):
         if (len(self.albums) != 1 or
                 self.albums[0].name != self.TEST_ALBUM or
                 self.albums[0].count != "3"):
-            print "[Regenerating Albums]"
+            print "\n[Regenerating Albums]"
             self._delete_all()
             self._create_test_photos()
             self.photos = self.client.photos.list()
