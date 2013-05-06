c4ea338f49718aaf424278a166af557161b77c48
sneakypete81 <sneakypete81@gmail.com>
2013-05-06 21:27:31 +0100

    Workarounds for APIv1 issues

--- tests/api_versions/test_v1.py
+++ tests/api_versions/test_v1.py
@@ -1,3 +1,4 @@
+import unittest
 from tests import test_albums, test_photos, test_tags
 
 class TestAlbumsV1(test_albums.TestAlbums):
@@ -6,5 +7,6 @@ class TestAlbumsV1(test_albums.TestAlbums):
 class TestPhotosV1(test_photos.TestPhotos):
     api_version = 1
 
-class TestTagsV1(test_tags.TestTags):
-    api_version = 1
+# The tag API didn't work at v1 - see frontend issue #927
+# class TestTagsV1(test_tags.TestTags):
+    # api_version = 1
